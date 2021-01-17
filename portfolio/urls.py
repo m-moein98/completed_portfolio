ef7558b95930373b5ad264c7a3e0076f49c45b57
mohammed moein <moein1475963.mmz@gmail.com>
2021-01-17 08:45:32 +0330

    'url bug fixed'

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -25,5 +25,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('api/blog/', include('blog.urls')),
-    re_path(r'blog/', TemplateView.as_view(template_name ="blog.html")),
+    path('blog/', TemplateView.as_view(template_name ="blog.html")),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
