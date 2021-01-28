90f917754596b9363b4b27241cc0417fc74888cf
mohammed moein <moein1475963.mmz@gmail.com>
2021-01-28 10:50:26 +0330

    'model update'

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,3 +5,5 @@ class Messages(models.Model):
     name = models.CharField(max_length = 60,default="")
     contact_id = models.CharField(max_length=60,default="")
     message = models.TextField(default="")
+    def __str__(self):
+        return self.name
